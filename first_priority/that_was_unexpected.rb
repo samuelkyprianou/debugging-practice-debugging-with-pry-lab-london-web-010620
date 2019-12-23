fbf3bd123a809236774f9f15d56324adc70155d7
Sam Kyprianou <samuelkyprianou@gmail.com>
2019-12-23 18:05:06 +0000

    Automatically backed up by Learn

--- first_priority/that_was_unexpected.rb
+++ first_priority/that_was_unexpected.rb
@@ -2,11 +2,11 @@ require 'pry'
 
 class Person
   attr_accessor :name, :candy_lover
+    binding.pry
 
   def initialize(name, candy_lover)
     @name = name
     @candy_lover = candy_lover
-    binding.pry
   end
 end
 
